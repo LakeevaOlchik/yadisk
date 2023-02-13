0f4d74b6e3b9172b68c357f625296238f74e7e6c
Olga L <lakeeva.olchik@yandex.by>
2023-02-13 16:19:13 +0300

    firstcommit

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+heroes_list = ['Hulk', 'Captain america', 'Thanos']
+intelligence_dict = {'Hulk': 0, 'Captain america': 0, 'Thanos': 0}
+url = 'https://www.superheroapi.com/api.php/2619421814940190/search/'
+
+for hero in heroes_list:
+    hero_dict = json.loads(requests.get(url + hero).content)
+    intelligence_dict[hero] = int(hero_dict['results'][0]['powerstats']['intelligence'])
+
+print(f'Самый умный супергерой {max(intelligence_dict)}')
